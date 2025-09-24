495b379d4092e67413d707ed8faf144a00a5f443
panic0021 <panic.veljko@gmail.com>
2025-09-24 20:23:59 +0200

    Dodati seederi za grad,knjige,pretplata

---
+++ biblioteka-backend/database/seeders/ZanrSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ZanrSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
