0839aff82937361b7feb56965ad14f65a6795c45
panic0021 <panic.veljko@gmail.com>
2025-09-28 22:54:50 +0200

    Dodate rute koje zahtevaju autentifikaciju

--- biblioteka-backend/bootstrap/app.php
+++ biblioteka-backend/bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => \App\Http\Middleware\AdminPristup::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
