156559df8dee5ab6ac614fdf58911ed897a9808b
JanaStevanovic <janastevanovic68@gmail.com>
2025-10-01 16:11:05 +0200

    Dodata početna stranica

--- biblioteka-frontend/src/App.js
+++ biblioteka-frontend/src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Meni from "./komponente/Meni";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Pocetna from "./stranice/Pocetna";
+import Knjige from "./stranice/Knjige";
+import Onama from "./stranice/Onama";
+import MojeKnjige from "./stranice/MojeKnjige";
+import Admin from "./stranice/Admin";
+import Login from "./stranice/Login";
+import {Container} from "react-bootstrap";
+import Footer from "./komponente/Footer";
+import Donatori from "./stranice/Donatori";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Meni />
+        <div className="background" >
+            <Container>
+                <div className="main">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Pocetna />} />
+                            <Route path="/donatori" element={<Donatori />} />
+                            <Route path="/knjige" element={<Knjige />} />
+                            <Route path="/onama" element={<Onama />} />
+                            <Route path="/moje-knjige" element={<MojeKnjige />} />
+                            <Route path="/admin" element={<Admin />} />
+                            <Route path="/login" element={<Login />} />
+
+                        </Routes>
+                    </BrowserRouter>
+                </div>
+            </Container>
+        </div>
+
+        <Footer />
+
+    </>
   );
 }
 
